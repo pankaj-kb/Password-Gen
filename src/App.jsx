5af64912df5b0ecadca6c07db47e9e3a1ec64eee
Pankaj Bhadane <pankajb2601@gmail.com>
2023-10-23 22:01:18 +0530

    added rpassword gen function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import { useState, useCallback } from "react";
 
 import "./App.css";
 
@@ -8,9 +10,21 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
-  const passwordGenerator = () => {
-    
-  }
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let chars = "!@#$%^&*_-+=:;.?/|~";
+    let nums = "0123456789";
+
+    if (numAllowed) str += nums;
+    if (charAllowed) str += chars;
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+  }, [length, numAllowed, charAllowed, setPassword]);
 
   return (
     <>
